d5af5dbe6fcc83c1a54a0820eb5b1bd60529619c
James Harris <jimeharrisjr@gmail.com>
2022-05-10 15:50:34 -0400

    Added ability to specify SVD components directly, cleaned up the code for readability

--- R/DMD.R
+++ R/DMD.R
@@ -7,6 +7,7 @@ kdmd<-function(x){
 #'
 #' @param data the matrix data for which you wish to predict future columns
 #' @param p The Percentage of explanation of the SVD required (default=1)
+#' @param comp The number of components of the SVD to keep. Default is null, overrides p, if set.
 #' Returns an object of class kdmd
 #' @examples
 #' ## Not run:
@@ -14,27 +15,34 @@ kdmd<-function(x){
 #' m[1,]<-1:10
 #' m[2,]<-2:11
 #' m[3,]<-3,12
-#' A<-getAmatrix(m)
+#' A<-getAMatrix(m)
 #' @importFrom pracma pinv
 #' @export
-getAMatrix<-function(data,p=1){
+getAMatrix<-function(data,p=1, comp=NULL){
   x<-data[,-ncol(data)]
   y<-data[,-1]
   wsvd<-base::svd(x)
-  sv<-(wsvd$d^2)/sum(wsvd$d^2)
-  if (p==1){
-    r<-length(sv)
+  if (!is.null(comp)){
+    r<-comp
+    if (comp>length(wsvd$d)){
+      warning('Specified number of components is greater than possible. Ignoring extra')
+      r<-length(wsvd$d)
+    }
   } else {
-    r<-max(which(cumsum(sv)>=p)[1],2)
+    if (p==1){
+      r<-length(wsvd$d)
+    } else {
+      sv<-(wsvd$d^2)/sum(wsvd$d^2)
+      r<-max(which(cumsum(sv)>=p)[1],2)
+    }
   }
-
   u<-wsvd$u
   v<-wsvd$v
   d<-wsvd$d
-  Atil2<-crossprod(u[,1:r],y)
-  Atil3<-crossprod(t(Atil2),v[,1:r])
-  AtilR<-crossprod(t(Atil3),diag(1/d[1:r]))
-  eig<-eigen(AtilR)
+  Atil<-crossprod(u[,1:r],y)
+  Atil<-crossprod(t(Atil),v[,1:r])
+  Atil<-crossprod(t(Atil),diag(1/d[1:r]))
+  eig<-eigen(Atil)
   Phi<-eig$values
   Q<-eig$vectors
   Psi<- y %*% v[,1:r] %*% diag(1/d[1:r]) %*% (Q)
@@ -49,7 +57,6 @@ getAMatrix<-function(data,p=1){
 #' @param A the Koopman Matrix for prediction
 #' @param data the matrix data for which you wish to predict future columns
 #' @param l the length of the predictions (number of columns to predict)
-#' @param p The Percentage of explanation of the SVD required (default=1)
 #' Returns a matrix with l additional columns
 #' @examples
 #' ## Not run:
@@ -58,11 +65,12 @@ getAMatrix<-function(data,p=1){
 #' m[1,]<-1:10
 #' m[2,]<-2:11
 #' m[3,]<-3:12
-#' A<-getAmatrix(m)
+#' A<-getAMatrix(m)
 #' predict(A,m,1)
 #' @importFrom pracma pinv
 #' @export
-predict.kdmd<-function(A,data,l,p=1){
+predict.kdmd<-function(A,data,l){
+  if (!'kdmd' %in% class(A)){ stop('A must be a kdmd object generated with getAMatrix')}
   len_predict<-l
   t<-dim(data)[2]
   N<-dim(data)[1]
